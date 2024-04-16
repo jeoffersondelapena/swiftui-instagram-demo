dcdc4eba9fcacd8a93d1c31d4d30867d25ce1f50
Jeofferson Dela Peña <jeoffersond@gmail.com>
2024-04-16 11:24:25 +0800

    Initial Commit

---
+++ swiftui-instagram-demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-instagram-demo
+//
+//  Created by Jeofferson Dela Peña on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-instagram-demo/swiftui_instagram_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_instagram_demoApp.swift
+//  swiftui-instagram-demo
+//
+//  Created by Jeofferson Dela Peña on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_instagram_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
